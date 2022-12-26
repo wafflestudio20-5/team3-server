f360b7844ab3eb384454df10aba35ceaf42280e3
leeeryboy <indiv0227@snu.ac.kr>
2022-12-26 20:34:04 +0900

    Feat: 프론트 연결용 컨트롤러 생성

---
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/controller/UserController.kt
@@ -0,0 +1,14 @@
+package com.wafflestudio.team03server.core.user.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController {
+
+    @GetMapping("/hello")
+    fun hello():String{
+        return "hello"
+    }
+
+}
