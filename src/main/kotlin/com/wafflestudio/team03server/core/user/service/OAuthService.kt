9a5fd62a6b707e19c602ae079c163da3a78d6555
jwahoonkim <kjhoon0330@snu.ac.kr>
2022-12-29 02:05:36 +0900

    Feat: oauth util class 구현

---
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/service/OAuthService.kt
@@ -0,0 +1,22 @@
+package com.wafflestudio.team03server.core.user.service
+
+import com.wafflestudio.team03server.core.user.controller.response.GoogleLoginResponse
+import com.wafflestudio.team03server.core.user.controller.vo.GoogleAuthToken
+import com.wafflestudio.team03server.core.user.controller.vo.GoogleUser
+import com.wafflestudio.team03server.utils.GoogleOAuth
+import org.springframework.stereotype.Service
+import javax.servlet.http.HttpServletResponse
+
+@Service
+class OAuthService(
+    private val googleOAuth: GoogleOAuth,
+    private val response: HttpServletResponse,
+) {
+
+    fun oAuthLogin(code: String): GoogleLoginResponse {
+        val googleAuthToken: GoogleAuthToken = googleOAuth.getAccessToken(code)
+        val googleUser: GoogleUser = googleOAuth.getUserInfo(googleAuthToken);
+        val userEmail: String = googleUser.email
+        return GoogleLoginResponse("zxcuasc.asxauisx.asdxas", 100, googleAuthToken.access_token, googleAuthToken.token_type)
+    }
+}
