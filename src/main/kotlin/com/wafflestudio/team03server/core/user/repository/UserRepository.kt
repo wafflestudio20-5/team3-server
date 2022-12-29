bd7339112786039e7c5e6a90cbaaeaebd496863a
jwahoonkim <kjhoon0330@snu.ac.kr>
2022-12-29 20:14:42 +0900

    Feat: UserRepository 이메일로 검색 구현

---
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.wafflestudio.team03server.core.user.repository
+
+import com.wafflestudio.team03server.core.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(email: String): User?
+}
