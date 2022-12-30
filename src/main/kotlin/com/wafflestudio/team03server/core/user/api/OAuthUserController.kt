675a6fbd8db6331beae0a08de0b678a8ab221892
jwahoonkim <kjhoon0330@snu.ac.kr>
2022-12-30 22:43:02 +0900

    Feat: branch 이동을 위한 OAuth Service, Controller 커밋

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/OAuthUserController.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/OAuthUserController.kt
@@ -1,30 +1,46 @@
-package com.wafflestudio.team03server.core.user.controller
+package com.wafflestudio.team03server.core.user.api
 
-import com.wafflestudio.team03server.core.user.controller.response.GoogleLoginResponse
+import com.wafflestudio.team03server.core.user.api.request.GoogleLoginRequest
+import com.wafflestudio.team03server.core.user.api.response.GoogleLoginResponse
+import com.wafflestudio.team03server.core.user.service.AuthTokenService
 import com.wafflestudio.team03server.core.user.service.OAuthService
 import com.wafflestudio.team03server.utils.GoogleOAuth
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
+import javax.validation.Valid
 
 @RestController
-@RequestMapping("/app/accounts/auth")
 class OAuthUserController(
     private val googleOAuth: GoogleOAuth,
     private val oAuthService: OAuthService,
+    private val authTokenService: AuthTokenService,
     private val response: HttpServletResponse,
 ) {
+    val AUTHORIZATION_HEADER: String = "Authorization"
 
-    @GetMapping("/google")
-    fun googleLogin() {
-        val redirectUrl: String = googleOAuth.getOAuthRedirectUrl()
-        response.sendRedirect(redirectUrl)
-    }
+//    @GetMapping("/app/accounts/auth/google")
+//    fun googleLogin() {
+//        val redirectUrl: String = googleOAuth.getOAuthRedirectUrl()
+//        response.sendRedirect(redirectUrl)
+//    }
+//
+//    @GetMapping("/app/accounts/auth/google/callback")
+//    fun googleLoginCallback(@RequestParam(name = "code") code: String): GoogleLoginResponse {
+//        return oAuthService.oAuthLogin(code)
+//    }
 
-    @GetMapping("/google/callback")
-    fun googleLoginCallback(@RequestParam(name = "code") code: String): GoogleLoginResponse {
-        return oAuthService.oAuthLogin(code)
+    @PostMapping("/google/login")
+    fun googleLogin(@Valid @RequestBody googleLoginRequest: GoogleLoginRequest): ResponseEntity<GoogleLoginResponse> {
+        val userEmail = googleLoginRequest.email
+        val response = oAuthService.googleLogin(userEmail)
+        val jwtToken = authTokenService.generateTokenByEmail(userEmail).accessToken
+        val headers = HttpHeaders()
+        headers.set(AUTHORIZATION_HEADER, jwtToken)
+        return ResponseEntity(response, headers, HttpStatus.OK)
     }
 }
--- src/main/kotlin/com/wafflestudio/team03server/core/user/service/OAuthService.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/service/OAuthService.kt
@@ -1,25 +1,38 @@
 package com.wafflestudio.team03server.core.user.service
 
-import com.wafflestudio.team03server.core.user.controller.response.GoogleLoginResponse
-import com.wafflestudio.team03server.core.user.controller.vo.GoogleAuthToken
-import com.wafflestudio.team03server.core.user.controller.vo.GoogleUser
+import com.wafflestudio.team03server.common.Exception404
+import com.wafflestudio.team03server.core.user.api.response.GoogleLoginResponse
+import com.wafflestudio.team03server.core.user.api.response.SimpleUserResponse
+import com.wafflestudio.team03server.core.user.repository.UserRepository
 import com.wafflestudio.team03server.utils.GoogleOAuth
 import org.springframework.stereotype.Service
+
+private const val NEED_SIGNUP_MESSAGE = "회원가입이 필요합니다."
 
 @Service
 class OAuthService(
     private val googleOAuth: GoogleOAuth,
+    private val userRepository: UserRepository,
 ) {
 
-    fun oAuthLogin(code: String): GoogleLoginResponse {
-        val googleAuthToken: GoogleAuthToken = googleOAuth.getAccessToken(code)
-        val googleUser: GoogleUser = googleOAuth.getUserInfo(googleAuthToken)
-        val userEmail: String = googleUser.email
-        return GoogleLoginResponse(
-            "zxcuasc.asxauisx.asdxas",
-            100,
-            googleAuthToken.access_token,
-            googleAuthToken.token_type
-        )
+//    fun oAuthLogin(code: String): GoogleLoginResponse {
+//        val googleAuthToken: GoogleAuthToken = googleOAuth.getAccessToken(code)
+//        val googleUser: GoogleUser = googleOAuth.getUserInfo(googleAuthToken)
+//        val userEmail: String = googleUser.email
+//        return GoogleLoginResponse(
+//            "zxcuasc.asxauisx.asdxas",
+//            100,
+//            googleAuthToken.access_token,
+//            googleAuthToken.token_type
+//        )
+//    }
+
+    fun googleLogin(email: String): GoogleLoginResponse {
+        val findUser = userRepository.findByEmail(email) ?: throw Exception404(NEED_SIGNUP_MESSAGE)
+        if (!findUser.emailVerified) {
+            userRepository.delete(findUser)
+            throw Exception404(NEED_SIGNUP_MESSAGE)
+        }
+        return GoogleLoginResponse(SimpleUserResponse.of(findUser))
     }
 }
