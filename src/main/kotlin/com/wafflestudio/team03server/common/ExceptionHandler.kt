cc42bf93b715c64637f56cab2de0669df2cc4c8a
leeeryboy <indiv0227@snu.ac.kr>
2022-12-31 20:59:44 +0900

    Feat: Validation 관련 exception handler 추가

--- src/main/kotlin/com/wafflestudio/team03server/common/ExceptionHandler.kt
+++ src/main/kotlin/com/wafflestudio/team03server/common/ExceptionHandler.kt
@@ -1,12 +1,20 @@
 package com.wafflestudio.team03server.common
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandler {
 
-    @ExceptionHandler(value = [RuntimeException::class])
+    @ExceptionHandler(value = [Exception::class])
     fun handle(e: Exception) = ResponseEntity<ErrorResponse>(ErrorResponse(e.message!!), e.status)
+
+    // Request Validation
+    @ExceptionHandler(value = [MethodArgumentNotValidException::class])
+    fun handle(e: MethodArgumentNotValidException): ResponseEntity<Any> {
+        return ResponseEntity(e.bindingResult.allErrors[0].defaultMessage, HttpStatus.BAD_REQUEST)
+    }
 }
