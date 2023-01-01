d578f5e5b26efac8e5c6a2ae5008142b5b0799c1
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-01-01 18:29:59 +0900

    Feat: Kakao 로그인을 위한 Data Class 구현

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/response/LoginResponse.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/response/LoginResponse.kt
@@ -1,6 +1,6 @@
 package com.wafflestudio.team03server.core.user.api.response
 
-data class GoogleLoginResponse(
+data class LoginResponse(
     val jwtToken: String,
-    val user: SimpleUserResponse
+    val user: SimpleUserResponse,
 )
