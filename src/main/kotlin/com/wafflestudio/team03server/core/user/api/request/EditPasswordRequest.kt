9263c9391d6a085f0f707d4ec4c92ed66a2a0c09
leeeryboy <indiv0227@snu.ac.kr>
2023-01-03 01:55:36 +0900

    Feat: Request Validation

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditPasswordRequest.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditPasswordRequest.kt
@@ -1,10 +1,12 @@
 package com.wafflestudio.team03server.core.user.api.request
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
 
 data class EditPasswordRequest(
     val password:String,
-    @field:NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,15}\$",
+        message = "비밀번호는 특수문자, 영문자, 숫자 포함 8~15자 여야 합니다")
     val newPassword:String?,
     val newPasswordConfirm:String
 )
--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/LoginRequest.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/LoginRequest.kt
@@ -3,8 +3,6 @@ package com.wafflestudio.team03server.core.user.api.request
 import javax.validation.constraints.NotBlank
 
 data class LoginRequest(
-    @field:NotBlank(message = "email is required")
-    val email: String?,
-    @field:NotBlank(message = "password is required")
-    val password: String?
+    val email: String,
+    val password: String
 )
