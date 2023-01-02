e9a3b265630f446393bbdbfc22cdf6c25052a300
leeeryboy <indiv0227@snu.ac.kr>
2023-01-02 23:41:56 +0900

    Feat: 유저 정보 수정 기능

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/UserController.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/UserController.kt
@@ -2,14 +2,14 @@ package com.wafflestudio.team03server.core.user.api
 
 import com.wafflestudio.team03server.common.Authenticated
 import com.wafflestudio.team03server.common.UserContext
+import com.wafflestudio.team03server.core.user.api.request.EditPasswordRequest
+import com.wafflestudio.team03server.core.user.api.request.EditProfileRequest
 import com.wafflestudio.team03server.core.user.api.response.UserResponse
 import com.wafflestudio.team03server.core.user.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/users")
@@ -19,12 +19,25 @@ class UserController(
 
     @GetMapping("/{user-id}")
     fun getUser(@PathVariable("user-id") userId: Long): ResponseEntity<UserResponse> {
-        return ResponseEntity(userService.getUser(userId),HttpStatus.OK)
+        return ResponseEntity(userService.getProfile(userId),HttpStatus.OK)
     }
 
     @Authenticated
     @GetMapping("/me")
     fun getMe(@UserContext userId: Long): ResponseEntity<UserResponse>{
-        return ResponseEntity(userService.getUser(userId),HttpStatus.OK)
+        return ResponseEntity(userService.getProfile(userId),HttpStatus.OK)
+    }
+
+    @Authenticated
+    @PatchMapping("/me")
+    fun editMe(@UserContext userId: Long, @Valid @RequestBody editProfileRequest: EditProfileRequest): ResponseEntity<UserResponse>{
+        return ResponseEntity(userService.editProfile(userId,editProfileRequest),HttpStatus.OK)
+    }
+
+    @Authenticated
+    @PutMapping("/me/password")
+    fun editPassword(@UserContext userId:Long,@Valid @RequestBody editPasswordRequest: EditPasswordRequest ):ResponseEntity<Any>{
+        userService.editPassword(userId,editPasswordRequest)
+        return ResponseEntity(HttpStatus.OK)
     }
 }
