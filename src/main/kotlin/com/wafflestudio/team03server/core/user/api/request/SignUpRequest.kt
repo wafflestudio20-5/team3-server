b2d20ea0c8d8c2439ca91ede2f08391a5527df32
leeeryboy <indiv0227@snu.ac.kr>
2023-01-03 14:03:33 +0900

    Fix: 정규식 수정

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/SignUpRequest.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/SignUpRequest.kt
@@ -8,15 +8,15 @@ import javax.validation.constraints.Pattern
 data class SignUpRequest(
     @field:Pattern(
         regexp = "^([a-zA-Z0-9가-힣]){2,10}\$",
-        message = "유저네임은 영문자, 숫자, 한글 중 하나 이상을 포함해야 하며, 1~10자 여야 합니다."
+        message = "유저네임은 영문자, 숫자, 한글 중 하나 이상을 포함해야 하며, 2~10자 여야 합니다."
     )
     val username: String?,
     @field:Email(message = "이메일 형식이 올바르지 않습니다.")
     @field:NotBlank(message = "이메일은 필수 항목입니다.")
     val email: String?,
     @field:Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,15}\$",
-        message = "비밀번호는 특수문자, 영문자, 숫자 포함 8~15자 여야 합니다"
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,20}\$",
+        message = "비밀번호는 특수문자, 영문자, 숫자 포함 8~20자 여야 합니다"
     )
     val password: String?,
     @field:NotBlank(message = "주소는 필수 항목입니다.")
