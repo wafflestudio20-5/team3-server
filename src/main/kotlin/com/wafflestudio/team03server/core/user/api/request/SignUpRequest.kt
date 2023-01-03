b74db58e8b6421f8ebc6497bef14ca106793c827
leeeryboy <indiv0227@snu.ac.kr>
2023-01-03 14:00:15 +0900

    Fix: 정규식 수정

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/SignUpRequest.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/SignUpRequest.kt
@@ -7,7 +7,7 @@ import javax.validation.constraints.Pattern
 
 data class SignUpRequest(
     @field:Pattern(
-        regexp = "^[\\p{L}\\p{Nd}가-힣]{1,10}\$",
+        regexp = "^([a-zA-Z0-9가-힣]){2,10}\$",
         message = "유저네임은 영문자, 숫자, 한글 중 하나 이상을 포함해야 하며, 1~10자 여야 합니다."
     )
     val username: String?,
