e93fc3d2f0a0122e51bb49ba829d91a804099989
Junhyeong Kim <indiv0227@snu.ac.kr>
2023-01-07 23:14:30 +0900

    Feat: 프로필 이미지 업로드 기능 구현 (#20)
    * Feat: AWS S3 파일 업로드
    
    * Feat: 유저 프로필 이미지 업로드 기능 구현
    
    * Style: ktlint
    
    * Remove: 불필요한 라인 삭제

---
+++ src/main/kotlin/com/wafflestudio/team03server/config/AwsConfig.kt
@@ -0,0 +1,32 @@
+package com.wafflestudio.team03server.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.wafflestudio.team03server.core.user.service.S3Properties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(S3Properties::class)
+class AwsConfig(
+    private val s3Properties: S3Properties
+) {
+
+    private val accessKey = s3Properties.credentials.accessKey
+
+    private val secretKey = s3Properties.credentials.secretKey
+
+    private val region = s3Properties.region.static
+
+    @Bean
+    fun amazonS3(): AmazonS3 {
+        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build()
+    }
+}
