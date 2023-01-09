a0f02ab96d1790cc9148f9f6ff771a0f942e38f2
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-01-09 18:16:56 +0900

    Chore: Websocket을 위한 설정 추가

---
+++ src/main/kotlin/com/wafflestudio/team03server/config/WebSockConfig.kt
@@ -0,0 +1,23 @@
+package com.wafflestudio.team03server.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSockConfig : WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry
+            .addEndpoint("/ws-stomp")
+            .setAllowedOriginPatterns("*")
+            .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/sub")
+        registry.setApplicationDestinationPrefixes("/pub")
+    }
+}
