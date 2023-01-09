68d6ab6cbfbdbf65b56a68c4d901358366ac0b5b
leeeryboy <indiv0227@snu.ac.kr>
2023-01-09 17:25:27 +0900

    Refactor: 유저 정보 수정 Request 분리

---
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditLocationRequest.kt
@@ -0,0 +1,8 @@
+package com.wafflestudio.team03server.core.user.api.request
+
+import javax.validation.constraints.NotBlank
+
+data class EditLocationRequest(
+    @field:NotBlank(message = "주소는 필수 항목입니다.")
+    val location:String?
+)
--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditPasswordRequest.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditPasswordRequest.kt
@@ -5,8 +5,8 @@ import javax.validation.constraints.Pattern
 data class EditPasswordRequest(
     val password: String,
     @field:Pattern(
-        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,15}\$",
-        message = "비밀번호는 특수문자, 영문자, 숫자 포함 8~15자 여야 합니다"
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@\$!%*#?&])[A-Za-z\\d@\$!%*#?&]{8,20}\$",
+        message = "비밀번호는 특수문자, 영문자, 숫자 포함 8~20자 여야 합니다"
     )
     val newPassword: String?,
     val newPasswordConfirm: String
