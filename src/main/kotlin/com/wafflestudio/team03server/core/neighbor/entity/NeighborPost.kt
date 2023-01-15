1b62a80ed220ddb4057de0c9aac4c388b22f375e
곽민지 <beinte0419@snu.ac.kr>
2023-01-15 21:16:00 +0900

    Refactor: OneToMany필드 batch size 적용

--- src/main/kotlin/com/wafflestudio/team03server/core/neighbor/entity/NeighborPost.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/neighbor/entity/NeighborPost.kt
@@ -2,6 +2,7 @@ package com.wafflestudio.team03server.core.neighbor.entity
 
 import com.wafflestudio.team03server.common.BaseTimeEntity
 import com.wafflestudio.team03server.core.user.entity.User
+import org.hibernate.annotations.BatchSize
 import javax.persistence.*
 
 @Entity
@@ -13,9 +14,11 @@ class NeighborPost(
     @JoinColumn(name = "publisher_id")
     val publisher: User,
 
+    @BatchSize(size=100)
     @OneToMany(mappedBy = "neighborPost", cascade = [CascadeType.ALL])
     var comments: MutableList<NeighborComment> = mutableListOf(),
 
+    @BatchSize(size=100)
     @OneToMany(mappedBy = "likedPost", cascade = [CascadeType.ALL])
     var likes: MutableList<NeighborLike> = mutableListOf(),
 
