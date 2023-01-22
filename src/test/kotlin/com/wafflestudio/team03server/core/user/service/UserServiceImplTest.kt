d56391f52dc31a8aebf6badea8f571a7b7748a14
leeeryboy <indiv0227@snu.ac.kr>
2023-01-22 22:33:12 +0900

    Test: 유저 서비스 테스트

--- src/test/kotlin/com/wafflestudio/team03server/core/user/service/UserServiceImplTest.kt
+++ src/test/kotlin/com/wafflestudio/team03server/core/user/service/UserServiceImplTest.kt
@@ -1,17 +1,27 @@
 package com.wafflestudio.team03server.core.user.service
 
+import com.wafflestudio.team03server.common.Exception400
+import com.wafflestudio.team03server.common.Exception403
+import com.wafflestudio.team03server.common.Exception404
+import com.wafflestudio.team03server.common.Exception409
 import com.wafflestudio.team03server.core.chat.service.ChatService
 import com.wafflestudio.team03server.core.trade.api.request.CreatePostRequest
 import com.wafflestudio.team03server.core.trade.repository.TradePostRepository
 import com.wafflestudio.team03server.core.trade.service.TradePostService
+import com.wafflestudio.team03server.core.user.api.request.EditLocationRequest
+import com.wafflestudio.team03server.core.user.api.request.EditPasswordRequest
+import com.wafflestudio.team03server.core.user.api.request.EditUsernameRequest
 import com.wafflestudio.team03server.core.user.entity.User
 import com.wafflestudio.team03server.core.user.repository.UserRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.transaction.annotation.Transactional
 
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -20,9 +30,9 @@ import org.springframework.transaction.annotation.Transactional
 internal class UserServiceImplTest @Autowired constructor(
     val userRepository: UserRepository,
     val userService: UserService,
-    val tradePostRepository: TradePostRepository,
     val tradePostService: TradePostService,
     val chatService: ChatService,
+    val passwordEncoder: PasswordEncoder
 ) {
 
     @Test
@@ -33,13 +43,14 @@ internal class UserServiceImplTest @Autowired constructor(
         //when
         val userResponse = userService.getProfile(savedUser.id)
         //then
-        assertThat(user.username).isEqualTo(userResponse.username)
-        assertThat(user.email).isEqualTo(userResponse.email)
-        assertThat(user.location).isEqualTo(userResponse.location)
-        assertThat(user.temperature).isEqualTo(userResponse.temperature)
-        assertThat(user.imgUrl).isEqualTo(userResponse.imgUrl)
-        assertThat(user.createdAt).isEqualTo(userResponse.createdAt)
-        assertThat(user.modifiedAt).isEqualTo(userResponse.modifiedAt)
+        val findUser = userRepository.findByIdOrNull(savedUser.id) ?: throw Exception404("")
+        assertThat(findUser.username).isEqualTo(userResponse.username)
+        assertThat(findUser.email).isEqualTo(userResponse.email)
+        assertThat(findUser.location).isEqualTo(userResponse.location)
+        assertThat(findUser.temperature).isEqualTo(userResponse.temperature)
+        assertThat(findUser.imgUrl).isEqualTo(userResponse.imgUrl)
+        assertThat(findUser.createdAt).isEqualTo(userResponse.createdAt)
+        assertThat(findUser.modifiedAt).isEqualTo(userResponse.modifiedAt)
     }
 
     @Test
@@ -47,47 +58,102 @@ internal class UserServiceImplTest @Autowired constructor(
         //given
 
         //when
-        val userResponse = userService.getProfile(1)
+        val exception = assertThrows(Exception404::class.java) {
+            userService.getProfile(1)
+        }
         //then
-
+        assertThat(exception.message).isEqualTo("사용자를 찾을 수 없습니다.")
     }
 
     @Test
-    fun editUsername() {
+    fun 유저네임_수정_성공() {
         //given
-
+        val user = User("user1", "a@naver.com", "1234", "송도동")
+        val savedUser = userRepository.save(user)
         //when
-
+        userService.editUsername(savedUser.id, EditUsernameRequest("Edited"))
         //then
+        val findUser = userRepository.findByIdOrNull(savedUser.id) ?: throw Exception404("")
+        assertThat(findUser.username).isEqualTo("Edited")
     }
 
     @Test
-    fun editLocation() {
+    fun 유저네임_수정_실패() {
         //given
-
+        val user1 = User("user1", "a@naver.com", "1234", "송도동")
+        val user2 = User("user2", "b@naver.com", "1234", "송도동")
+        val savedUser1 = userRepository.save(user1)
+        val savedUser2 = userRepository.save(user2)
         //when
-
+        val exception = assertThrows(Exception409::class.java) {
+            userService.editUsername(savedUser1.id, EditUsernameRequest("user2"))
+        }
         //then
+        assertThat(exception.message).isEqualTo("이미 존재하는 유저네임 입니다.")
     }
 
     @Test
-    fun editPassword() {
+    fun 주소_수정_성공() {
         //given
-
+        val user = User("user1", "a@naver.com", "1234", "송도동")
+        val savedUser = userRepository.save(user)
         //when
-
+        userService.editLocation(savedUser.id, EditLocationRequest("Edited"))
         //then
+        val findUser = userRepository.findByIdOrNull(savedUser.id) ?: throw Exception404("")
+        assertThat(findUser.location).isEqualTo("Edited")
     }
 
     @Test
-    fun uploadImage() {
+    fun 비밀번호_수정_성공() {
         //given
-
+        val user = User("user1", "a@naver.com", "1234", "송도동")
+        user.password = passwordEncoder.encode(user.password)
+        val savedUser = userRepository.save(user)
         //when
-
+        userService.editPassword(
+            savedUser.id,
+            EditPasswordRequest("1234", "Edited123!", "Edited123!")
+        )
         //then
+        val findUser = userRepository.findByIdOrNull(savedUser.id) ?: throw Exception404("")
+        passwordEncoder.matches("Edited123!", findUser.password)
     }
 
+    @Test
+    fun 비밀번호_수정_실패_비밀번호_틀림() {
+        //given
+        val user = User("user1", "a@naver.com", "1234", "송도동")
+        user.password = passwordEncoder.encode(user.password)
+        val savedUser = userRepository.save(user)
+        //when
+        val exception = assertThrows(Exception403::class.java) {
+            userService.editPassword(
+                savedUser.id,
+                EditPasswordRequest("wrongPw", "Edited123!", "Edited123!")
+            )
+        }
+        //then
+        assertThat(exception.message).isEqualTo("기존 비밀번호가 틀렸습니다.")
+    }
+
+    @Test
+    fun 비밀번호_수정_실패_새비밀번호_불일치() {
+        //given
+        val user = User("user1", "a@naver.com", "1234", "송도동")
+        user.password = passwordEncoder.encode(user.password)
+        val savedUser = userRepository.save(user)
+        //when
+        val exception = assertThrows(Exception400::class.java) {
+            userService.editPassword(
+                savedUser.id,
+                EditPasswordRequest("1234", "Edited123!", "NotIdentical!")
+            )
+        }
+        //then
+        assertThat(exception.message).isEqualTo("비밀번호가 일치하지 않습니다.")
+    }
+    
     @Test
     fun 글_구매_내역_조회_성공() {
         // given
