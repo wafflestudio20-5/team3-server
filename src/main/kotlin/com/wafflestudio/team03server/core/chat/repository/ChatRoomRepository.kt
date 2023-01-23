dcb02fcae5b70ce5c79cba6686137db54da6ddec
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-01-23 22:24:18 +0900

    Feat: MyChats 기능 구현

--- src/main/kotlin/com/wafflestudio/team03server/core/chat/repository/ChatRoomRepository.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/chat/repository/ChatRoomRepository.kt
@@ -5,7 +5,7 @@ import com.wafflestudio.team03server.core.trade.entity.TradePost
 import com.wafflestudio.team03server.core.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ChatRoomRepository : JpaRepository<ChatRoom, Long> {
+interface ChatRoomRepository : JpaRepository<ChatRoom, Long>, ChatRoomCustomRepository {
     fun findChatRoomByBuyerAndSellerAndPost(buyer: User, seller: User, post: TradePost): ChatRoom?
     fun findChatRoomByRoomUUID(roomUUID: String): ChatRoom?
 }
