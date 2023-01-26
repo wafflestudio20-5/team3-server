76c78acd31b531bf7940a231e03e41aed7e7bfba
Jahni <kjhoon0330@snu.ac.kr>
2023-01-26 17:08:30 +0900

    Fix: MyChats 목록에서 내가 구매자인 경우 제대로 표시되지 않는 버그 수정 (#64)
    * Chore: [endpoint 변경] products -> tradepost
    
    * Fix: Like 연관관계 메서드 추가 및 테스트 추가
    
    * Fix: MyChats 목록에서 내가 구매자인 경우 제대로 표시되지 않는 버그 수정

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/response/MyChatsResponse.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/response/MyChatsResponse.kt
@@ -3,14 +3,15 @@ package com.wafflestudio.team03server.core.user.api.response
 import com.wafflestudio.team03server.core.chat.api.response.ChatHistoryResponse
 import com.wafflestudio.team03server.core.chat.entity.ChatRoom
 import com.wafflestudio.team03server.core.trade.api.response.PostResponse
+import com.wafflestudio.team03server.core.user.entity.User
 
 data class MyChatsResponse(
     val chats: List<DetailChatResponse>
 ) {
     companion object {
-        fun of(chatRooms: List<ChatRoom>): MyChatsResponse {
+        fun of(me: User, chatRooms: List<ChatRoom>): MyChatsResponse {
             return MyChatsResponse(
-                chats = chatRooms.map { DetailChatResponse.of(it) },
+                chats = chatRooms.map { DetailChatResponse.of(me, it) },
             )
         }
     }
@@ -18,19 +19,23 @@ data class MyChatsResponse(
 
 data class DetailChatResponse(
     val roomUUID: String,
-    val buyer: SimpleUserResponse,
+    val you: SimpleUserResponse,
     val post: PostResponse,
     val lastChat: ChatHistoryResponse?,
 ) {
     companion object {
-        fun of(chatRoom: ChatRoom): DetailChatResponse {
+        fun of(me: User, chatRoom: ChatRoom): DetailChatResponse {
             return DetailChatResponse(
                 roomUUID = chatRoom.roomUUID,
-                buyer = SimpleUserResponse.of(chatRoom.buyer),
-                post = PostResponse.of(chatRoom.post, chatRoom.seller),
+                you = SimpleUserResponse.of(getYou(chatRoom, me)),
+                post = PostResponse.of(chatRoom.post, me),
                 // OneToMany 관계인데 그 중 하나만 가져오고싶지만.. 다 가져온뒤 하나 선택
                 lastChat = getLastChatResponseOrNull(chatRoom),
             )
+        }
+
+        private fun getYou(chatRoom: ChatRoom, me: User): User {
+            return if (chatRoom.buyer == me) chatRoom.seller else chatRoom.buyer
         }
 
         private fun getLastChatResponseOrNull(chatRoom: ChatRoom): ChatHistoryResponse? {
