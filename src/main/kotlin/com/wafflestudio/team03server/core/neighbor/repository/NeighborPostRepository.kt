85180dbe8f79ccf8edc66429ff8d0ba42b337c30
곽민지 <beinte0419@snu.ac.kr>
2023-01-30 00:50:32 +0900

    Chore: 사용하지 않는 NeighborPostSupport 삭제

--- src/main/kotlin/com/wafflestudio/team03server/core/neighbor/repository/NeighborPostRepository.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/neighbor/repository/NeighborPostRepository.kt
@@ -1,17 +1,12 @@
 package com.wafflestudio.team03server.core.neighbor.repository
 
-import com.querydsl.jpa.impl.JPAQueryFactory
 import com.wafflestudio.team03server.core.neighbor.entity.NeighborPost
-import com.wafflestudio.team03server.core.neighbor.entity.QNeighborLike.neighborLike
-import com.wafflestudio.team03server.core.neighbor.entity.QNeighborPost.neighborPost
-import com.wafflestudio.team03server.core.user.entity.QUser.user
 import org.locationtech.jts.geom.Point
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import org.springframework.stereotype.Component
 
-interface NeighborPostRepository : JpaRepository<NeighborPost, Long>, NeighborPostSupport {
+interface NeighborPostRepository : JpaRepository<NeighborPost, Long> {
     @Query(
         value = "SELECT SQL_CALC_FOUND_ROWS p.*, ST_Distance_Sphere(u.coordinate, :point) " +
             "as distance FROM neighbor_post p JOIN users u ON p.publisher_id = u.id WHERE p.content " +
@@ -51,36 +46,4 @@ interface NeighborPostRepository : JpaRepository<NeighborPost, Long>, NeighborPo
         @Param("offset") offset: Long,
         @Param("liker_id") liker_id: Long
     ): List<NeighborPost>
-}
-
-interface NeighborPostSupport {
-    fun findAllByPublisherId(publisherId: Long): List<NeighborPost>
-    fun findAllByLikerId(likerId: Long): List<NeighborPost>
-}
-
-@Component
-class NeighborPostSupportImpl(
-    private val queryFactory: JPAQueryFactory
-) : NeighborPostSupport {
-    override fun findAllByPublisherId(publisherId: Long): List<NeighborPost> {
-        return queryFactory
-            .selectFrom(neighborPost)
-            .leftJoin(neighborPost.publisher, user)
-            .fetchJoin()
-            .where(neighborPost.publisher.id.eq(publisherId))
-            .distinct()
-            .fetch()
-    }
-
-    override fun findAllByLikerId(likerId: Long): List<NeighborPost> {
-        return queryFactory
-            .selectFrom(neighborPost)
-            .leftJoin(neighborPost.publisher, user)
-            .fetchJoin()
-            .leftJoin(neighborLike)
-            .on(neighborPost.id.eq(neighborLike.likedPost.id))
-            .where(neighborLike.liker.id.eq(likerId))
-            .distinct()
-            .fetch()
-    }
 }
