492ab6132fe77ddd17145a936c1f1bccf8fc2a31
leeeryboy <indiv0227@snu.ac.kr>
2023-02-01 00:19:15 +0900

    Fix: 주소 변경 요청에 좌표 정보도 포함

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditLocationRequest.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/request/EditLocationRequest.kt
@@ -1,8 +1,12 @@
 package com.wafflestudio.team03server.core.user.api.request
 
+import com.wafflestudio.team03server.core.user.entity.Coordinate
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class EditLocationRequest(
     @field:NotBlank(message = "주소는 필수 항목입니다.")
-    val location: String?
+    val location: String?,
+    @field:NotNull(message = "좌표 정보가 없습니다.")
+    val coordinate: Coordinate?
 )
