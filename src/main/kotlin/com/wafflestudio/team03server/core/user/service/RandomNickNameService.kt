ce5469d30fdc869cac1cb4402ee7b38ee60a1c23
jwahoonkim <kjhoon0330@snu.ac.kr>
2023-02-01 22:43:04 +0900

    Feat: 랜덤닉네임 기능 구현

---
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/service/RandomNickNameService.kt
@@ -0,0 +1,34 @@
+package com.wafflestudio.team03server.core.user.service
+
+import com.wafflestudio.team03server.core.user.api.response.RandomNickNameResponse
+import com.wafflestudio.team03server.core.user.api.vo.RandomNickNameVO
+import com.wafflestudio.team03server.core.user.repository.UserRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+
+@Service
+class RandomNickNameService(
+    val userRepository: UserRepository,
+) {
+    fun getRandomNickname(): RandomNickNameResponse {
+        val randomNickname = findRandomNickNameWhileNotDuplicated()
+        return RandomNickNameResponse(randomNickname)
+    }
+
+    private fun findRandomNickNameWhileNotDuplicated(): String {
+        var randomNickName = requestRandomNickname()
+        while (userRepository.findByUsername(randomNickName) != null) {
+            randomNickName = requestRandomNickname()
+        }
+        return randomNickName
+    }
+
+    private fun requestRandomNickname(): String {
+        val restTemplate = RestTemplate()
+        val res = restTemplate.getForEntity(
+            "https://nickname.hwanmoo.kr/?format=json&max_length=10",
+            RandomNickNameVO::class.java
+        )
+        return res.body!!.words[0].filterNot { it.isWhitespace() }
+    }
+}
