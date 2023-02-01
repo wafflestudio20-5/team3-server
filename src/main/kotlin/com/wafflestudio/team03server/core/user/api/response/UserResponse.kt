812e5f2a9a8463418b7030f5bd4f07f3e5e78582
leeeryboy <indiv0227@snu.ac.kr>
2023-02-02 00:46:26 +0900

    Fix: 유저 프로필 응답에 검색 범위 추가

--- src/main/kotlin/com/wafflestudio/team03server/core/user/api/response/UserResponse.kt
+++ src/main/kotlin/com/wafflestudio/team03server/core/user/api/response/UserResponse.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.team03server.core.user.api.response
 
+import com.wafflestudio.team03server.core.user.entity.SearchScope
 import com.wafflestudio.team03server.core.user.entity.User
 import java.time.LocalDateTime
 
@@ -8,6 +9,7 @@ data class UserResponse(
     val username: String,
     val email: String,
     val location: String,
+    val searchScope: SearchScope,
     val temperature: Double,
     val imgUrl: String?,
     val createdAt: LocalDateTime,
@@ -20,6 +22,7 @@ data class UserResponse(
                 username = user.username,
                 email = user.email,
                 location = user.location,
+                searchScope = user.searchScope,
                 temperature = user.temperature,
                 imgUrl = user.imgUrl,
                 createdAt = user.createdAt!!,
